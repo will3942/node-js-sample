81441ecec5472019954bf378b0aa4707f7e3e8ae
Will Evans <will@will3942.com>
2015-04-17 00:21:18 +0100

    Added disco test

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express')
+var request = require('request')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
@@ -8,6 +9,16 @@ app.get('/', function(request, response) {
   response.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+var container = {
+  "id": process.env.CONTAINER_NAME,
+  "type": "tcp",
+  "port": 5000,
+  "service": { "id": "node-js-sample" }
+};
+
+var url = process.env.DISCO_BASE_URI + "/1/containers";
+request({ url: url, method: 'POST', json: container}, function(err, response, body) {
+	app.listen(app.get('port'), function() {
+	  console.log("Node app is running at localhost:" + app.get('port'))
+	})
+});
